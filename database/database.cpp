ed2ab953fc18f723417dbcb90336c983703d93d0
Vladislav Milovanov <vm@gusi-lebedi.ru>
2024-11-22 04:25:21 +0300

    updated template's placement

--- database/database.cpp
+++ database/database.cpp
@@ -9,15 +9,27 @@ namespace
 std::string getConnectionString(const slk::DatabaseSettings& settings)
 {
     auto connectionString = std::format("host={} port={} dbname={} user={} password={} ",
-                                              settings.host,
-                                              settings.port,
-                                              settings.dbName,
-                                              settings.user,
-                                              settings.password);
+                                        settings.host,
+                                        settings.port,
+                                        settings.dbName,
+                                        settings.user,
+                                        settings.password);
 
     connectionString += settings.requireSsl ? "sslmode=require" : "sslmode=disable";
 
     return connectionString;
+}
+
+template<class T>
+void setValue(T& value, const pqxx::field& fieldValue)
+{
+    value = fieldValue.as<T>();
+}
+
+template<class... ReturnFields,  std::size_t... Is>
+void getRow(std::tuple<ReturnFields...>& row, const pqxx::row& resRow, const std::index_sequence<Is...>)
+{
+    return (setValue(std::get<Is>(row), resRow[Is]), ...);
 }
 
 } //! Utils namespace
@@ -71,10 +83,31 @@ void Database::prepare(const std::string& name, const std::string& statement)
     impl().connection->prepare(name, statement);
 }
 
-const std::unique_ptr<pqxx::connection>& Database::connection() const
+
+template<class... ReturnFields, class... Params>
+bool Database::execute(const std::string& query, std::vector<std::tuple<ReturnFields...>>& returnRows, const Params&... params)
 {
-    return impl().connection;
+    try {
+        pqxx::work worker(*impl().connection);
+
+        const auto result = sizeof...(Params) > 0 ? worker.exec_params(query, params...)
+                                                  : worker.exec(query);
+
+        worker.commit();
+
+        std::ranges::for_each(result, [&returnRows](const pqxx::row& resRow) {
+            std::tuple<ReturnFields...> row;
+            getRow(row, resRow, std::make_index_sequence<sizeof...(ReturnFields)>{});
+            returnRows.push_back(row);
+        });
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
+template bool Database::execute<int>(const std::string&, std::vector<std::tuple<int>>&);
 
 } //! slk
--- database/database.h
+++ database/database.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 #include <pqxx/pqxx>
 
 #include "pimpl.h"
@@ -21,43 +19,8 @@ public:
     void prepare(const std::string& name, const std::string& statement);
 
     template<class... ReturnFields, class... Params>
-    bool execute(const std::string& query, std::vector<std::tuple<ReturnFields...>>& returnRows, const Params&... params)
-    {
-        try {
-            pqxx::work worker(*connection());
+    bool execute(const std::string& query, std::vector<std::tuple<ReturnFields...>>& returnRows, const Params&... params);
 
-            const auto result = sizeof...(Params) > 0 ? worker.exec_params(query, params...)
-                                                      : worker.exec(query);
-
-            worker.commit();
-
-            std::ranges::for_each(result, [this, &returnRows](const pqxx::row& resRow) {
-                std::tuple<ReturnFields...> row;
-                getRow(row, resRow, std::make_index_sequence<sizeof...(ReturnFields)>{});
-                returnRows.push_back(row);
-            });
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-            return false;
-        }
-
-        return true;
-    }
-
-private:
-    template<class... ReturnFields,  std::size_t... Is>
-    void getRow(std::tuple<ReturnFields...>& row, const pqxx::row& resRow, const std::index_sequence<Is...>)
-    {
-        return (setValue(std::get<Is>(row), resRow[Is]), ...);
-    }
-
-    template<class T>
-    void setValue(T& value, const pqxx::field& fieldValue)
-    {
-        value = fieldValue.as<T>();
-    }
-
-    const std::unique_ptr<pqxx::connection>& connection() const;
 private:
     DECLARE_PIMPL
 };
